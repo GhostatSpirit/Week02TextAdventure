ad4b82b3dfce1cf05b4f2de59a9b0c3d29c74cfa
Yang Liu <lykavin@hotmail.com>
2016-07-13 10:42:03 -0400

    Implement Space Station and part of the bar.

--- Assets/Scripts/TextAdvController.cs
+++ Assets/Scripts/TextAdvController.cs
@@ -3,37 +3,42 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class TextAdvController : MonoBehaviour {
-	// member variables
+	// member variables:
+
+	// string that stores the current room name
 	string currentRoom;
+	// bool that decides if the hero can enter the bar
+	bool hasBarAccess;
 
 	// Use this for initialization
 	void Start () {
 		currentRoom = "Intro";
+		hasBarAccess = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		string textBuffer = "Hi. \n";
+		// declare the text buffer
+		string textBuffer = "";
 
 		if (currentRoom == "Intro") {
 			ShowIntro (ref textBuffer);
+
 		}
-		else if (currentRoom == "Elevators"){
+		else if (currentRoom == "Space Station"){
 			ShowRoomName(ref textBuffer);
-			textBuffer += "You see the security guard.\n";
-
-			textBuffer += "press [W] to go to elevators\n";
-			textBuffer += "press [S] to go outside\n";
-
-			if (Input.GetKeyDown(KeyCode.W)){
-				currentRoom = "Elevators";
-			} else if (Input.GetKeyDown(KeyCode.S)){
-				currentRoom = "Outside";
-			}
-
+			ShowStationIntro (ref textBuffer);
 		}
-		else if (currentRoom == "Outside"){
-			// all your OUTSIDE code
+		else if (currentRoom == "Bar"){
+			ShowRoomName (ref textBuffer);
+			if(!hasBarAccess){
+				ShowBarBlocked (ref textBuffer);
+			} else{
+				ShowBarEnter (ref textBuffer);
+			}
+		}
+		else if (currentRoom == "Market"){
+			ShowMarket (ref textBuffer);
 		}
 
 		// render out the text buffer
@@ -43,7 +48,7 @@ public class TextAdvController : MonoBehaviour {
 
 	// add a line that shows the room name too the buffer
 	void ShowRoomName(ref string textBuffer){
-		string temp = "You are currently in " + currentRoom + ". \n";
+		string temp = "You are currently in: " + currentRoom + "\n";
 		textBuffer += temp;
 	}
 
@@ -53,9 +58,61 @@ public class TextAdvController : MonoBehaviour {
 		textBuffer += "You are Spike Spiegel.\n";
 		textBuffer += "You are a space cowboy, a bounty hunter.\n";
 		textBuffer += "\n";
-		textBuffer += "When you don't have a target, your life is usually boring, sometimes frugal.\n";
+
+		textBuffer += "When you don't have a target, your life is usually boring.\n";
+		textBuffer += "But not this time...";
 		textBuffer += "\n";
-		textBuffer += "But not this time.";
-		//textBuffer += "";
+
+		textBuffer += "You are chasing a most-wanted, a dangerous dynamite scientist called Decker.\n";
+		textBuffer += "All the evidence lead you here... Mars. The beautiful planet.\n";
+		textBuffer += "\n";
+
+		textBuffer += "press [SPACE] to land on Mars Space Station…\n";
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			currentRoom = "Space Station";
+		}
+
+	}
+
+	void ShowStationIntro(ref string textBuffer){
+		textBuffer +=   "Mars is the new hangout for humanity, " +
+						"after the earth is heavily damaged by an accident " +
+						"during the construction of Astral Gate.\n" +
+						"\nMars Space Station is the transportation center. " +
+						"This place is jammed with space shutters " +
+						"which can take you anywhere on Mars, even to those borderlands.\n" +
+						"\nWhen you are tracing someone, bars and markets are the places to go.\n" +
+						"\npress [A] to go to the Bounty Hunter Bar…\n" +
+						"press [D] to head for Mars Open Market…\n";
+
+		if(Input.GetKeyDown(KeyCode.A)){
+			currentRoom = "Bar";
+		} else if(Input.GetKeyDown(KeyCode.D)){
+			currentRoom = "Market";
+		}
+
+	} 
+
+	void ShowBarBlocked(ref string textBuffer){
+		textBuffer +=   "As soon as you step onto the stairs in front of Bounty Hunter Bar, " +
+						"two sturdy guards get in your way and speak firmly:\n" +
+						"“This is an restricted area. VIPs only.”\n" +
+						"\n“Hah, a Bounty Hunter Bar blocks bounty hunters away,” you think.\n" +
+						"As you are thinking, your space shutter takes off and flies away.\n" +
+						"Now you have to wait hours for another shutter.\n" +
+						"\npress [S] to head back to Mars Space Station…";
+		
+		if(Input.GetKeyDown(KeyCode.S))
+			currentRoom = "Space Station";
+
+	}
+
+	void ShowBarEnter(ref string textBuffer){
+		
+	}
+
+	void ShowMarket (ref string textBuffer){
+		
 	}
 }
